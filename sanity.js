bdab95ca1780c076209bb4fe47dc89fafbf3cb46
Rohan <007rohanprasad@gmail.com>
2022-05-20 15:43:04 +0530

    first commit

---
+++ sanity.js
@@ -0,0 +1,13 @@
+import { createCureentUserHook, createClient } from 'next-sanity'
+import creatImageUrlBuilder from '@sanity/image-url'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'uhulm6nb',
+  apiVersion: '2021-03-25',
+  useCdn: process.env.NODE_ENV === 'production',
+}
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (source) => creatImageUrlBuilder(config).image(source)
